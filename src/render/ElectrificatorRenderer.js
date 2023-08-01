955d53e8a519e22b810ce9b8a6bb8e7d14f5cb5b
Minier Arnaud <arnaud.minier@telecom-paris.fr>
2023-08-01 18:05:55 +0200

    WIP: Basic renderer

--- src/render/ElectrificatorRenderer.js
+++ src/render/ElectrificatorRenderer.js
@@ -1,10 +1,36 @@
-import { DefaultRender } from 'leto-modelizer-plugin-core';
+import nunjucks from 'nunjucks';
+import templates from 'src/render/ElectrificatorTemplate';
+import { DefaultRender, FileInput } from 'leto-modelizer-plugin-core';
 
 /**
  * Template of plugin renderer.
  */
 class ElectrificatorRenderer extends DefaultRender {
-  renderFiles() {
+  constructor(pluginData) {
+    super(pluginData);
+
+    const Loader = nunjucks.Loader.extend({
+      getSource(name) {
+        return {
+          src: templates[name],
+        };
+      },
+    });
+    const env = new nunjucks.Environment(new Loader(), {
+      autoescape: false,
+      trimBlocks: true,
+      lstripBlocks: true,
+    });
+    this.template = nunjucks.compile(templates.root, env);
+  }
+
+  /**
+   * Convert all provided components and links in terraform files.
+   *
+   * @param {string} [parentEventId] - Parent event id.
+   * @returns {FileInput[]} Array of generated files from components and links.
+   */
+  renderFiles(parentEventId = null) {
     /*
      * The purpose of this function is to generate all files.
      *
@@ -14,7 +40,64 @@ class ElectrificatorRenderer extends DefaultRender {
      *
      * Components can be find in `this.pluginData.components`.
      */
-    return []; // Return FileInput[].
+    // return []; // Return FileInput[].
+
+    return this.generateFilesFromComponentsMap(
+      this.pluginData.components.reduce(
+        (map, component) => {
+          if (!map.has(component.path)) {
+            map.set(component.path, [component]);
+          } else {
+            map.get(component.path).push(component);
+          }
+          return map;
+        },
+        new Map(),
+      ),
+      parentEventId,
+    );
+  }
+
+  /**
+   * Render files from related components.
+   *
+   * @param {Map<string,Component>} map - Component mapped by file name.
+   * @param {string} parentEventId - Parent event id.
+   * @returns {FileInput[]} Render files array.
+   */
+  generateFilesFromComponentsMap(map, parentEventId) {
+    const files = [];
+
+    map.forEach((components, path) => {
+      const id = this.pluginData.emitEvent({
+        parent: parentEventId,
+        type: 'Render',
+        action: 'write',
+        status: 'running',
+        files: [path],
+        data: {
+          global: false,
+        },
+      });
+      console.log(components);
+      console.log(path);
+      const generated_content = components.forEach((component) => {
+        this.generateContentFromComponent(component);
+      });
+      files.push(new FileInput({
+        path,
+        // content: `${this.template.render({ components }).trim()}\n`,
+        content: JSON.stringify(components),
+      }));
+
+      this.pluginData.emitEvent({ id, status: 'success' });
+    });
+
+    return files;
+  }
+
+  generateContentFromComponent(component) {
+    return JSON.stringify(component);
   }
 }
 
---
+++ src/render/ElectrificatorTemplate.js
@@ -0,0 +1,25 @@
+const root = `{% for _block in components %}
+{{ _block.definition.blockType }} {% if ['resource','data'].includes(_block.definition.blockType) %}"{{ _block.definition.type }}" {% endif %}{% if ['provider', 'module'].includes(_block.definition.blockType) %}"{{ _block.definition.type }}"{% else %}"{{ _block.id }}"{% endif %} {
+{% for attribute in _block.attributes %}{% set level = 1 %}
+{% include "attribute" ignore missing %}
+{% endfor %}
+}
+
+{% endfor %}`;
+
+const attribute = `{% if attribute.type == 'Object' %}
+{{ attribute.name | indent(level * 4, true) }} {% if not attribute.isDynamic %}= {% endif %}{
+{% set level = level+1 %}{% for attr in attribute.value %}{% set attribute = attr %}
+{% include "attribute" ignore missing %}
+{% set attribute = attr %}
+{% endfor %}{% set level = level-1 %}
+{{ "}" | indent(level * 4, true)  }}
+{% else %}
+{{ attribute.name | indent(level * 4, true)}} = {{ attribute.value | dump }}
+{% endif %}
+`;
+
+export default {
+  root,
+  attribute,
+};
