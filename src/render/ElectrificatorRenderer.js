4f505683e1f87df792492469e4896a607a0258c1
Minier Arnaud <arnaud.minier@telecom-paris.fr>
2023-08-02 15:18:26 +0200

    Working renderer with block composition

--- src/render/ElectrificatorRenderer.js
+++ src/render/ElectrificatorRenderer.js
@@ -61,12 +61,15 @@ class ElectrificatorRenderer extends DefaultRender {
   /**
    * Render files from related components.
    *
-   * @param {Map<string,Component>} map - Component mapped by file name.
+   * @param {Map<string,Component[]>} map - Components mapped by file name.
    * @param {string} parentEventId - Parent event id.
    * @returns {FileInput[]} Render files array.
    */
   generateFilesFromComponentsMap(map, parentEventId) {
     const files = [];
+    const parsedComponents = [];
+
+    console.log(map);
 
     map.forEach((components, path) => {
       const id = this.pluginData.emitEvent({
@@ -81,13 +84,17 @@ class ElectrificatorRenderer extends DefaultRender {
       });
       console.log(components);
       console.log(path);
-      const generated_content = components.forEach((component) => {
-        this.generateContentFromComponent(component);
+      components.forEach((component) => {
+        this.parseComponent(parsedComponents, component);
       });
+      console.log(parsedComponents);
+      const generatedContentFromParsedComponents = parsedComponents.reduce((acc, value) => acc.concat(JSON.stringify(value)), '');
+
+      console.log(generatedContentFromParsedComponents);
       files.push(new FileInput({
         path,
         // content: `${this.template.render({ components }).trim()}\n`,
-        content: JSON.stringify(components),
+        content: generatedContentFromParsedComponents,
       }));
 
       this.pluginData.emitEvent({ id, status: 'success' });
@@ -96,8 +103,130 @@ class ElectrificatorRenderer extends DefaultRender {
     return files;
   }
 
-  generateContentFromComponent(component) {
-    return JSON.stringify(component);
+  /**
+   * Append content dict to container objects.
+   *
+   * @param {object[]} componentList - List of components.
+   * @param {Component} currentComponent - Current component.
+   */
+  parseComponent(componentList, currentComponent) {
+    let contentDict = {};
+    let parent = null;
+
+    if (currentComponent?.definition.type === 'containerObject') {
+      contentDict = {
+        name: currentComponent.id,
+        type: 'container',
+        attributes: {},
+        domain: 'electrical',
+        category: 'electrical_device',
+        description: currentComponent.definition.description,
+        objects: [],
+        links: [],
+        interfaces: [],
+      };
+      currentComponent?.attributes.forEach((attribute) => {
+        if (attribute.definition?.name === 'parent') {
+          parent = attribute.value;
+        }
+      });
+      this.parseContainerObject(componentList, parent, currentComponent.id, contentDict);
+    }
+
+    if (currentComponent?.definition.type === 'genericDipole') {
+      parent = 'root';
+      contentDict = {
+        name: currentComponent.id,
+        attributes: {
+          type: 'genericDipole',
+        },
+        domain: 'electrical',
+        category: 'electrical_device',
+        description: currentComponent.definition.description,
+      };
+      console.log(currentComponent?.attributes.values());
+      currentComponent?.attributes.forEach((attribute) => {
+        console.log(`attribute: ${attribute}`);
+        if (attribute.definition?.name === 'parent') {
+          parent = attribute.value;
+        }
+      });
+      this.appendContentDictToContainerObjects(componentList, parent, contentDict);
+    }
+  }
+
+  /**
+   * Get object from list by name.
+   *
+   * @param {object[]} objectList  List of objects
+   * @param {string} name Name of the object to be found
+   * @returns {null|object} Object with the name
+   */
+  getObjectFromListByName(objectList, name) {
+    return objectList.reduce((acc, object) => {
+      if (acc) {
+        return acc;
+      } if (object.name === name) {
+        return object;
+      }
+      return null;
+    }, null);
+  }
+
+  /**
+   * Append contentDict to the objects field of the object with the name
+   * parentObjectName in the objectList.
+   * If the object with the name parentObjectName does not exist in the objectList, it is created.
+   *
+   * @param {object[]} objectList List of objects
+   * @param {string} parentObjectName  Name of the object to which the contentDict is to be appended
+   * @param {object} contentDict Content to be appended
+   */
+  appendContentDictToContainerObjects(objectList, parentObjectName, contentDict) {
+    const containerObject = this.getObjectFromListByName(objectList, parentObjectName);
+    if (containerObject) {
+      containerObject.objects.push(contentDict);
+    } else {
+      objectList.push({
+        type: 'container',
+        name: parentObjectName,
+        objects: [contentDict],
+      });
+    }
+  }
+
+  /**
+   * Append contentDict to the objects field of the object with the name
+   *
+   * @param {object[]} objectList List of objects
+   * @param {string|null} parentObjectName Name of the object to which the contentDict
+   * is to be appended
+   * @param {string} childrenObjectName  Name of the object to which the contentDict belongs.
+   * @param {object} contentDict Content to be appended
+   */
+  parseContainerObject(
+    objectList,
+    parentObjectName,
+    childrenObjectName,
+    contentDict,
+  ) {
+    const currentObject = this.getObjectFromListByName(objectList, childrenObjectName);
+    // The current object already exists in the list,
+    // we have to remove it from the list and add it to the parent object
+    if (currentObject) {
+      objectList.splice(objectList.indexOf(currentObject), 1);
+      contentDict = {
+        ...currentObject,
+        ...contentDict,
+      };
+    }
+
+    const containerObject = this.getObjectFromListByName(objectList, parentObjectName);
+    if (containerObject) {
+      containerObject.objects.push(contentDict);
+    } else {
+      objectList.push(contentDict);
+    }
   }
 }
 
